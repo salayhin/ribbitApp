6096a908afbcf67d5dfcc06bf368feadd675f637
salayhin <salayhin@gmail.com>
2013-02-20 16:09:36 +0600

    Following other users is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 RibbitApp::Application.routes.draw do
+  resources :relationships
+
+
   resources :ribbits
-
-
-  get "sessions/new"
-
-  get "sessions/create"
-
-  get "sessions/destroy"
-
   resources :users
 
   root to: 'users#new'
